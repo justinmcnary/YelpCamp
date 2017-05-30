fc2b966b5474c6bb8d874b072110627f658a5a55
justinmcnary <justinmcnary@yahoo.com>
2017-05-30 15:24:58 -0400

    Comments and styles added

--- app.js
+++ app.js
@@ -2,39 +2,40 @@ let express  = require('express'),
   app        = express(),
   bodyParser = require('body-parser'),
   mongoose   = require('mongoose'),
-  request    = require('request')
+  request    = require('request'),
+  Campground = require('./models/campground'),
+  Comment    = require('./models/comment'),
+  seedDB     = require('./seed')
 
 mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public')) //underscore undersocre dirname
+seedDB();
 
-// SCHEMA SETUP
-let campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String
-});
-
-let Campground = mongoose.model('Campground', campgroundSchema);
 
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
+//Index Route
 app.get('/campgrounds', (req, res) => {
   //Geat all campgrounds
   Campground.find({}, (err, allCampgrounds)=> {
     if(err){
       console.log(`There was an error ${err}`);
     } else{
-      res.render('campgrounds', {campgrounds:allCampgrounds});
+      res.render('campgrounds/index', {campgrounds:allCampgrounds});
     }
   });
 });
 
+//Create add new campground to database
 app.post('/campgrounds', (req, res) => {
   let name = req.body.name;
   let image = req.body.image;
-  let newCampground = {name: name, image: image};
+  let desc = req.body.description;
+  let newCampground = {name: name, image: image, description: desc};
   //Create new campground and save to database
   Campground.create(newCampground, (err, newlyCreated) => {
     if(err){
@@ -45,14 +46,58 @@ app.post('/campgrounds', (req, res) => {
   });
 });
 
+//NEW displays form to make new campground
 app.get('/campgrounds/new', (req, res) => {
-  res.render('new');
+  res.render('campgrounds/new');
 });
 
-app.get('*', (req,res) => {
-  res.render('mistake');
+//shows info about one campground
+app.get('/campgrounds/:id', (req, res) => {
+  //find campground by ID then render show template
+  Campground.findById(req.params.id).populate('comments').exec((err, foundCampground) => {
+    if(err){
+      console.log(err);
+    } else {
+      res.render('campgrounds/show', {campground: foundCampground});
+    }
+  });
 });
 
-app.listen(3000, function() {
-  console.log('The server is live! on Port 3000.');
+// ====================
+// Comments Routes
+// =====================
+
+app.get('/campgrounds/:id/comments/new', (req, res) => {
+  //find campground by id
+  Campground.findById(req.params.id, (err, campground) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render('comments/new', {campground: campground});
+    }
+  });
+});
+
+app.post('/campgrounds/:id/comments', (req, res) => {
+//Lookup campground by id
+  Campground.findById(req.params.id, (err, campground) => {
+    if(err) {
+      console.log(err);
+      res.redirect('/campgrounds');
+    } else {
+      Comment.create(req.body.comment, (err, comment) => {
+        if(err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect(`/campgrounds/${campground._id}`);
+        }
+      })
+    };
+  })
+});
+
+app.listen(process.env.PORT || 3005, function() {
+  console.log('The server is live! on Port 3005.');
 })
---
+++ seed.js
@@ -0,0 +1,52 @@
+let mongoose = require('mongoose');
+let Campground = require('./models/campground');
+let Comment = require('./models/comment');
+
+let data = [
+  {name: 'Clouds Rest', image:'https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg',
+  description: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?'
+  },
+  {name: 'Hilly McHill', image:'https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg',
+    description: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?'
+  },
+  {name: 'Jupiters BrownEye', image:'https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg',
+    description: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?'
+    }
+]
+
+function seedDB() {
+  //Remove all campgrounds
+  Campground.remove({}, (err) => {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log('Removed Campgrounds!')
+    }
+  });
+  //add a few campgrounds
+  data.forEach((seed) => {
+    Campground.create(seed, (err, campground) => {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log('added a campground');
+        //create a comment
+        Comment.create(
+          {
+          text:'This place is great but could use WIFI!',
+          author: 'Homer'
+        }, (err, comment) => {
+          if(err) {
+            console.log(err);
+          } else {
+            campground.comments.push(comment);
+            campground.save();
+            console.log('Comment Created');
+          }
+        });
+      }
+    });
+  }); 
+};
+
+module.exports = seedDB;
